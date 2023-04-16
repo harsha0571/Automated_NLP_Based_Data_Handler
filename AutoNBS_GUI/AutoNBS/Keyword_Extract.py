6794adec72970f872c0f0a11f7843dcebed0c8ef
darklord-0 <singhharshit01010@gmail.com>
2023-04-16 18:47:42 +0530

    gui modified

---
+++ AutoNBS_GUI/AutoNBS/Keyword_Extract.py
@@ -0,0 +1,50 @@
+
+import time
+import json
+import spacy
+from keybert import KeyBERT
+kw_model = KeyBERT()
+nlp = spacy.load("en_core_web_sm")  # loads spacy's english core library
+sw_spacy = nlp.Defaults.stop_words
+
+
+def get_keywords_KeyBert(text, doc_location, filetype):
+
+    try:
+        keys = kw_model.extract_keywords(
+            docs=text,
+            keyphrase_ngram_range=(1, 1),
+            top_n=int(0.33*len(text))
+        )
+
+        if filetype == "img":
+            from autocorrect import Speller
+            for i, key in enumerate(keys):
+                spell = Speller().get_candidates(key[0])
+                if len(spell) == 1:
+                    keys[i] = (spell[0][1], key[1])
+
+        json_array = generate_json_keywords(keys, doc_location)
+        return json_array
+    except Exception as e:
+        with open('./AutoNBS/log.txt', 'a') as f:
+            f.write('\n\n'+time.ctime()+"\n"+str(e)+"\n\n")
+            f.close()
+        print("\nKeyword extraction failed hence skipped")
+
+
+def generate_json_keywords(keys, doc_location):
+    array = []
+    dict = {}
+    dict["doc_loc"] = doc_location
+    keywords = []
+    for key in keys:
+        keyDict = {}
+        if key[0] not in sw_spacy and key[1] > 0.2:
+            keyDict["keyword"] = key[0]
+            keyDict["score"] = key[1]
+            keywords.append(keyDict)
+
+    dict["keywords_array"] = keywords
+    array.append(dict)
+    return json.dumps(array)
--- AutoNBS_GUI/app.py
+++ AutoNBS_GUI/app.py
@@ -1,12 +1,11 @@
 import sys
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtCore import Qt,QTimer, QModelIndex, QAbstractTableModel
-from PyQt5.QtWidgets import QProgressBar
+from PyQt5.QtCore import Qt, QTimer, QModelIndex, QAbstractTableModel,QSize
 
 from PyQt5.QtWidgets import QDialogButtonBox, QApplication, QMainWindow, QListView, QAbstractItemView, QSplashScreen, QHeaderView
-from PyQt5.QtGui import QStandardItemModel, QStandardItem, QPixmap
+from PyQt5.QtGui import QStandardItemModel, QStandardItem, QPixmap,QIcon
 
-from Sample import Ui_MainWindow
+from sample_ui import Ui_MainWindow
 
 from codelib import *
 
@@ -15,6 +14,7 @@ import time
 import os
 import webbrowser
 import numpy as np
+
 
 class MyTableModel(QAbstractTableModel):
     def __init__(self, data=[[]], parent=None):
@@ -41,9 +41,10 @@ class MyTableModel(QAbstractTableModel):
             col = index.column()
             # print(type(self.data1[row][col]), self.data1[row][col])
             return str(self.data1[row][col])
-    
+
     def isUrl(self, index: QModelIndex):
-        return index.column()==1 
+        return index.column() == 1
+
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
@@ -51,53 +52,112 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         self.setupUi(self)
-        self.setFixedSize(840, 580)
+        self.setFixedSize(940, 680)
         self.listfiles = []
 
-        self.pixmap = QPixmap("gif.gif")
+        self.pixmap = QPixmap("./photos/loading.png")
         self.splash = QSplashScreen(self.pixmap)
+
+        pagebtnstyle = "QPushButton { background-color: #2980B9  ; color: white; border-radius:20px; border:2px solid  #522f02; } QPushButton:hover {background-color:#49abeb }"
+        page4btnstyle = "QPushButton {border-color: #000000);background-color: #ff9514; border-radius:15px;}  QPushButton:hover {background-color: #f7af57 }"
+        addkeybtnstyle = "QPushButton {  color : black; background-color: #27ff52 } QPushButton:hover {background-color: #71f58b }"
 
         self.addbtn.clicked.connect(self.filesGet)
         self.indexbtn.clicked.connect(self.startIndex)
         self.prevbtn.clicked.connect(self.changePage)
-        # self.label_1.setText("")
-        self.mainLabel.setMaximumSize(1000, 1000)
-        self.mainLabel.setStyleSheet("image : url(logo.png) ;background-position: center;background-repeat: no-repeat;")
-
         self.pushButton_2.clicked.connect(self.goSearch)
+        
+        self.addbtn.setStyleSheet(pagebtnstyle)
+        self.pushButton_2.setStyleSheet(pagebtnstyle)
+        self.prevbtn.setStyleSheet(pagebtnstyle)
+        self.indexbtn.setStyleSheet(pagebtnstyle)
+        self.addKey.setStyleSheet(addkeybtnstyle)
+        
+
+        self.goBackHome.setStyleSheet(page4btnstyle)
+        self.searchAgain.setStyleSheet(page4btnstyle)
+        
+        icon = QIcon("./photos/upload.png")
+        self.addbtn.setIcon(icon)
+        self.addbtn.setIconSize(QSize(40, 40))
+        self.addbtn.setText(" ADD FILES")
+        
+        icon = QIcon("./photos/home.png")
+        self.goBackHome.setIcon(icon)
+        self.goBackHome.setIconSize(QSize(40, 40))
+        self.goBackHome.setText("BACK TO HOME")
+        
+        icon = QIcon("./photos/searchagain.png")
+        self.searchAgain.setIcon(icon)
+        self.searchAgain.setIconSize(QSize(40, 40))
+        self.searchAgain.setText("SEARCH AGAIN")
+
+        icon = QIcon("./photos/search.png")
+        self.pushButton_2.setIcon(icon)
+        self.pushButton_2.setIconSize(QSize(40, 40))
+        self.pushButton_2.setText(" SEARCH FILES")
+        
+
+        icon = QIcon("./photos/plus.png")
+        self.addKey.setIcon(icon)
+        self.addKey.setIconSize(QSize(25,25))
+        self.addKey.setText(" ADD")
+        
+        icon = QIcon("./photos/left.png")
+        self.prevbtn.setIcon(icon)
+        self.prevbtn.setIconSize(QSize(30,30))
+        self.prevbtn.setText(" Previous")
+
+        self.mainLabel.setText("")
+        self.mainLabel.setMaximumSize(1000, 1000)
+        self.mainLabel.setStyleSheet("image : url(./photos/logo.png) ;background-position: center;background-repeat: no-repeat;")
 
         self.keyEntry.textChanged.connect(self.changeLabel)
-        self.addKey.clicked.connect(self.addKeywords)                                                           # to add the keyword from lineEdit text
+        # to add the keyword from lineEdit text
+        self.addKey.clicked.connect(self.addKeywords)
 
-        self.keyList= os.listdir('./AutoNBS/keywords/')
-        self.keyList= [i[:-8] for i in self.keyList]
-        self.model1= QStandardItemModel()                                                                       # model for availableKey listView, no multiple selection
+        self.keyList = os.listdir('./AutoNBS/keywords/')
+        self.keyList = [i[:-8] for i in self.keyList]
+
+        # model for availableKey listView, no multiple selection
+        self.model1 = QStandardItemModel()
         self.availableKeys.setModel(self.model1)
-        self.availableKeys.doubleClicked.connect(self.addKeyDirectly)                                           # signal and slot func for adding new keyword on double click
-        self.availableKeys.clicked[QModelIndex].connect(self.noticeKey)                                         # signal and slot func for deciding the key to be added
-        self.checkSuggestions.stateChanged.connect(self.provideSuggestions)                                     # to toggle keyword suggestion feature
+        # signal and slot func for adding new keyword on double click
+        self.availableKeys.doubleClicked.connect(self.addKeyDirectly)
+        # signal and slot func for deciding the key to be added
+        self.availableKeys.clicked[QModelIndex].connect(self.noticeKey)
+        # to toggle keyword suggestion feature
+        self.checkSuggestions.stateChanged.connect(self.provideSuggestions)
 
-        self.model = QStandardItemModel()                                                                       # model for selectedKey listView
-        self.mode= QAbstractItemView.MultiSelection                                                             # selection model for selectedKey listView
+        # model for selectedKey listView
+        self.model = QStandardItemModel()
+        # selection model for selectedKey listView
+        self.mode = QAbstractItemView.MultiSelection
         self.selectedKeys.setSelectionMode(self.mode)
         self.selectedKeys.setModel(self.model)
-        self.selectedKeys.clicked[QModelIndex].connect(self.on_clicked)                                         # to toggle the selection of keywords in selectedKeys
-        self.indexList= []                                                                                      # list of item index in selected list
+        # to toggle the selection of keywords in selectedKeys
+        self.selectedKeys.clicked[QModelIndex].connect(self.on_clicked)
+        # list of item index in selected list
+        self.indexList = []
 
-        self.buttonBox.button(QDialogButtonBox.Discard).clicked.connect(self.discard)
-        self.buttonBox.button(QDialogButtonBox.Close).clicked.connect(self.quit)      
-        self.buttonBox.button(QDialogButtonBox.Reset).clicked.connect(self.reset)   
-        self.buttonBox.button(QDialogButtonBox.Apply).clicked.connect(self.startSearch)             
+        self.buttonBox.button(
+            QDialogButtonBox.Discard).clicked.connect(self.discard)
+        self.buttonBox.button(
+            QDialogButtonBox.Close).clicked.connect(self.quit)
+        self.buttonBox.button(
+            QDialogButtonBox.Reset).clicked.connect(self.reset)
+        self.buttonBox.button(
+            QDialogButtonBox.Apply).clicked.connect(self.startSearch)
 
-        self.searchpixmap = QPixmap("./attempt1/wait.png")
-        self.searchsplash = QSplashScreen(self.searchpixmap)                                                                # splash screen resource     
+        self.searchpixmap = QPixmap("./photos/wait.png")
+        # splash screen resource
+        self.searchsplash = QSplashScreen(self.searchpixmap)
 
         self.fileLocations.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.fileLocations.doubleClicked.connect(self.OpenLink)      
+        self.fileLocations.doubleClicked.connect(self.OpenLink)
 
         self.goBackHome.clicked.connect(self.quit)
         self.searchAgain.clicked.connect(self.goSearch)
-
 
     def filesGet(self):
         self.listfiles = getFiles()
@@ -106,15 +166,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.stackedWidget.setCurrentIndex(1)
         self.listfiles = checkDuplicate(self.listfiles)
 
-        if len(self.listfiles)==0:
+        if len(self.listfiles) == 0:
             self.indexbtn.setEnabled(False)
-            self.label.setText("Duplicates file(s) found.\n\n Add Different file(s).")
+            self.label.setText(
+                "Duplicates file(s) found.\n\n Add Different file(s).")
         else:
             self.indexbtn.setEnabled(True)
-            self.label.setText("No Duplicates found.\n\n You can now start Indexing your files.")
-
-
-
+            self.label.setText(
+                "No Duplicates found.\n\n You can now start Indexing your files.")
 
     def startIndex(self):
         self.label.setText("Indexing your file(s)...")
@@ -122,7 +181,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.prevbtn.setEnabled(False)
         self.splash.show()
         QTimer.singleShot(1000, self.fileIndex)
-        
 
     def fileIndex(self):
         message = indexFiles(self.listfiles)
@@ -138,11 +196,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.indexbtn.setEnabled(True)
         self.prevbtn.setEnabled(True)
 
-
     def changePage(self):
         self.stackedWidget.setCurrentIndex(0)
-        # self.addbtn.setEnabled(True)
-
 
     def goSearch(self):
         self.reset()
@@ -150,29 +205,37 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # window.show()
 
     def changeLabel(self, text):
-        if text!="":                                                                                            # when no text is given to lineEdit initially, no suggestions
-            keySet= [name for name in self.keyList if text.lower() in name]                                     # list of keywords which have current text as substring
+        # when no text is given to lineEdit initially, no suggestions
+        if text != "":
+            # list of keywords which have current text as substring
+            keySet = [name for name in self.keyList if text.lower() in name]
             self.provideSuggestions(keySet=keySet)
         else:
             self.provideSuggestions(keySet=[])
-    
+
     def addKeywords(self):
-        text= self.keyEntry.text()
-        text= text.lower()                                                                                      # minimize risk with case sensitivity
-        self.model.appendRow(QStandardItem(text))                                                               # adding new keyword in selectedKey
-
-
-    def addKeyDirectly(self):                                                                                   # to add key from suggestions
-        text=self.model1.itemFromIndex(self.noticedKey).text()                                                  # converting ModelIndex obj to StandardItem to text
+        text = self.keyEntry.text()
+        # minimize risk with case sensitivity
+        text = text.lower()
+        # adding new keyword in selectedKey
         self.model.appendRow(QStandardItem(text))
 
-    def noticeKey(self, index):                                                                                 # to remember the keyword selected from availableKeys
-        self.noticedKey= index
+    # to add key from suggestions
+    def addKeyDirectly(self):
+        # converting ModelIndex obj to StandardItem to text
+        text = self.model1.itemFromIndex(self.noticedKey).text()
+        self.model.appendRow(QStandardItem(text))
 
-    def provideSuggestions(self, keySet= []):
+    # to remember the keyword selected from availableKeys
+    def noticeKey(self, index):
+        self.noticedKey = index
+
+    def provideSuggestions(self, keySet=[]):
         if self.checkSuggestions.isChecked:
-            self.model1.clear()                                                                                 # clear the list before appending data
-            if(type(keySet)!=int and len(keySet)>0):                                                            # add suggestive keywords if keySet is a list 
+            # clear the list before appending data
+            self.model1.clear()
+            # add suggestive keywords if keySet is a list
+            if (type(keySet) != int and len(keySet) > 0):
                 for i in keySet:
                     self.model1.appendRow(QStandardItem(i))
             else:
@@ -186,7 +249,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.indexList.append(index.row())
         else:
             self.indexList.remove(index.row())
-        print(self.indexList)
 
     def discard(self):
         self.indexList.sort(reverse=True)
@@ -195,7 +257,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         for i in self.indexList:
             self.selectedKeys.model().removeRow(i)
         self.indexList.clear()
-    
+
     def quit(self):
         self.stackedWidget.setCurrentIndex(0)
 
@@ -211,29 +273,30 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         QTimer.singleShot(2000, self.initiateSearch)
 
     def initiateSearch(self):
-        length= self.model.rowCount()
-        itemList= set()
+        length = self.model.rowCount()
+        itemList = set()
         for i in range(length):
-            item= self.selectedKeys.model().item(i).text()
+            item = self.selectedKeys.model().item(i).text()
             itemList.add(item)
             print(item)
-        keywords_list= list(itemList)
+        keywords_list = list(itemList)
         import Search_Module
 
         try:
             keywords_list = [key for key in keywords_list if key +
-                            ".parquet" in os.listdir("./AutoNBS/keywords")]
+                             ".parquet" in os.listdir("./AutoNBS/keywords")]
             start = time.time()
             result = Search_Module.searchForDocuments(
                 "./AutoNBS/keywords", keywords_list)
             end = time.time()
 
-            self.res= result.select("location").to_numpy()
+            self.res = result.select("location").to_numpy()
             print(os.path.basename(self.res[0][0]))
-            self.path= np.array(([os.path.basename(self.res[i][0]) for i in range(len(self.res))]))
-            self.path= self.path.reshape(len(self.path),1)
+            self.path = np.array(
+                ([os.path.basename(self.res[i][0]) for i in range(len(self.res))]))
+            self.path = self.path.reshape(len(self.path), 1)
             print(np.shape(self.path), np.shape(self.res))
-            self.res= np.hstack((self.path,self.res))
+            self.res = np.hstack((self.path, self.res))
 
             print("Result fetched in :", end-start, "sec")
 
@@ -242,20 +305,22 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.searchsplash.close()
             self.tablemodel = MyTableModel(self.res)
             self.fileLocations.setModel(self.tablemodel)
-            self.timeLabel.setText("Time taken to Search: "+str(end-start)+' seconds')
+            self.timeLabel.setText(
+                "Time taken to Search: \n"+str(end-start)+' seconds')
             # for i in range(len(self.res)):
             #     self.model.appendRow([QStandardItem(i+1), QStandardItem(self.res[i]) ])
         except Exception as e:
-        # with open('./AutoNBS/log.txt', 'a') as f:
-        #     f.write('\n\n'+time.ctime()+"\n"+str(e)+"\n\n")
-        #     f.close()
-            print("Search Failed\n",e)
+            # with open('./AutoNBS/log.txt', 'a') as f:
+            #     f.write('\n\n'+time.ctime()+"\n"+str(e)+"\n\n")
+            #     f.close()
+            print("Search Failed\n", e)
 
     def OpenLink(self, item):
-            # dat= 
-            if(self.tablemodel.isUrl(item)):
-                value= self.tablemodel.data(item, Qt.DisplayRole)
-                webbrowser.open(value)
+        # dat=
+        if (self.tablemodel.isUrl(item)):
+            value = self.tablemodel.data(item, Qt.DisplayRole)
+            webbrowser.open(value)
+
 
 app = QtWidgets.QApplication(sys.argv)
 
